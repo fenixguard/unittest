91c6ca502c22e66483152a87fb2eaa9b57495b49
Sergey Volkov <fenixguard1993@gmail.com>
2020-05-29 22:34:09 +0300

    added smoke test yandex translate

--- tests/test_yandex_api.py
+++ tests/test_yandex_api.py
@@ -2,13 +2,18 @@ import requests
 import unittest
 
 
-class YaTest(unittest.TestCase):
+class YandexTranslateTest(unittest.TestCase):
 
     def test_smoke(self):
-        resp = requests.get('http://yandex.ru')
-        self.assertEqual(resp.status_code, 200)
+        response = requests.get('https://translate.yandex.net/api/v1.5/tr.json/translate', params={
+            'key': 'trnsl.1.1.20200403T072622Z.1505b0f5f82fe645.8cc2ea18da93c3209773dc80aa5cdd1e03b60cc4',
+            'text': 'Hello World',
+            'lang': 'en-ru'
+        })
+        self.assertEqual(response.status_code, 200)
 
-        data = resp.content
-        self.assertNotEqual(data, '')
+        data = response.json()['text'][0]
+        self.assertEqual(data, 'Привет Мир')
+
 
 
